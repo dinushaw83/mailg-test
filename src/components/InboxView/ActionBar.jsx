107eccf788a2336c95cec48fdd2f612ca4356686
Benjamin Mayanja V <vibenjamin6@gmail.com>
2025-09-04 04:52:23 +0300

    feat(ui): refactor email details into InboxView component
    Refactor the email details page into a dedicated InboxView component structure with a separate ActionBar component. This improves code organization by moving UI logic from pages to components directory and adds action bar functionality to the email view.

---
+++ src/components/InboxView/ActionBar.jsx
@@ -0,0 +1,175 @@
+export const ActionBar = () => {
+  return (
+    <div className="D E G-atb" gh="tm">
+      <div className="G6" role="toolbar" aria-label="search refinement">
+        <div className="YhbRke sf-hidden" />
+      </div>
+      <div className="nH aqK">
+        <div className="Cq aqL" gh="mtb">
+          <div className="bzn" jslog="202616; u014N:xr6bB">
+            <div className="G-tF">
+              <div className="G-Ni J-J5-Ji">
+                <div
+                  id=":2u"
+                  className="T-I J-J5-Ji T-Pm T-I-ax7 L3 J-JN-M-I"
+                  role="button"
+                  tabIndex={0}
+                  aria-haspopup="true"
+                  aria-expanded="false"
+                  data-tooltip="Select"
+                  aria-label="Select"
+                  style={{ userSelect: "none" }}
+                >
+                  <div className="J-J5-Ji J-JN-M-I-Jm">
+                    <span
+                      className="T-Jo J-J5-Ji"
+                      jslog="170807; u014N:cOuCgd,Kr2w4b;"
+                      aria-checked="false"
+                      role="checkbox"
+                      dir="ltr"
+                      style={{ userSelect: "none" }}
+                    >
+                      <div className="T-Jo-auh sf-hidden" role="presentation" />
+                    </span>
+                    <div className="G-asx T-I-J3 J-J5-Ji" aria-hidden="true">
+                      &nbsp;
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="G-Ni J-J5-Ji">
+                <div
+                  className="T-I J-J5-Ji nu T-I-ax7 L3"
+                  act={20}
+                  role="button"
+                  tabIndex={0}
+                  jslog="110081; u014N:xr6bB,cOuCgd,Kr2w4b"
+                  data-tooltip="Refresh"
+                  aria-label="Refresh"
+                  style={{ userSelect: "none" }}
+                >
+                  <div className="asa">
+                    <div className="asf T-I-J3 J-J5-Ji" />
+                  </div>
+                </div>
+              </div>
+              <div className="J-J5-Ji">
+                <div className="T9" style={{ display: "none" }}>
+                  Fetching mail...
+                </div>
+              </div>
+              <div className="G-Ni J-J5-Ji">
+                <div
+                  id=":2w"
+                  className="T-I J-J5-Ji nf T-I-ax7 L3"
+                  role="button"
+                  tabIndex={0}
+                  aria-label="More email options"
+                  aria-haspopup="false"
+                  aria-expanded="false"
+                  data-tooltip="More"
+                  style={{ userSelect: "none" }}
+                >
+                  <div className="asa">
+                    <div className="bjy T-I-J3 J-J5-Ji" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="Cr aqJ">
+          <div className="ar5 J-J5-Ji">
+            <span className="Di">
+              <div
+                id=":mt"
+                className="J-J5-Ji amH J-JN-I"
+                role="button"
+                aria-expanded="false"
+                tabIndex={0}
+                aria-haspopup="false"
+                aria-label="Show more messages"
+                style={{ userSelect: "none" }}
+              >
+                <span className="Dj">
+                  <span>
+                    <span className="ts">1</span>–<span className="ts">50</span>{" "}
+                  </span>
+                  of <span className="ts">6,917</span>
+                </span>
+              </div>
+              <div
+                id=":mv"
+                className="T-I J-J5-Ji amD T-I-awG HeQuj T-I-ax7 T-I-Js-IF T-I-JE L3"
+                role="button"
+                jslog="126438; u014N:cOuCgd,Kr2w4b"
+                aria-disabled="true"
+                data-tooltip="Newer"
+                aria-label="Newer"
+                style={{ userSelect: "none" }}
+              >
+                <span className="amF sf-hidden" aria-hidden="true">
+                  &nbsp;
+                </span>
+                <img
+                  className="amI T-I-J3"
+                  src="/assets/images/pr_2_image_1.gif"
+                  alt=""
+                />
+              </div>
+              <div
+                id=":mw"
+                className="T-I J-J5-Ji amD T-I-awG T-I-ax7 T-I-Js-Gs L3"
+                role="button"
+                tabIndex={0}
+                jslog="126439; u014N:cOuCgd,Kr2w4b"
+                data-tooltip="Older"
+                aria-label="Older"
+                style={{ userSelect: "none" }}
+              >
+                <span className="amF sf-hidden" aria-hidden="true">
+                  &nbsp;
+                </span>
+                <img
+                  className="amJ T-I-J3"
+                  src="/assets/images/pr_2_image_1.gif"
+                  alt=""
+                />
+              </div>
+            </span>
+          </div>
+          <div
+            className="G-Ni J-J5-Ji"
+            jslog="177396; u014N:cOuCgd,Kr2w4b,xr6bB;"
+          >
+            <div
+              id=":1w"
+              className="T-I J-J5-Ji apF T-I-Js-IF T-I-ax7 L3"
+              role="button"
+              tabIndex={0}
+              data-tooltip="Toggle split pane mode"
+              aria-label="Toggle split pane mode"
+              style={{ userSelect: "none" }}
+            >
+              <div className="asa">
+                <div className="apH T-I-J3 J-J5-Ji apK" />
+              </div>
+            </div>
+            <div
+              id=":1y"
+              className="T-I J-J5-Ji T-I-Js-Gs apG T-I-ax7 L3"
+              role="button"
+              tabIndex={0}
+              aria-expanded="false"
+              aria-haspopup="true"
+              style={{ userSelect: "none" }}
+            >
+              <div className="G-asx J-J5-Ji" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
