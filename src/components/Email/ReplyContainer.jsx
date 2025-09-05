34f4c66a37afacfb4cf6119e85fde68b41482d17
samhirtb <samhir.tarif@gmail.com>
2025-09-06 01:31:09 +0500

    Structural changes to reply container

--- src/components/Email/ReplyContainer.jsx
+++ src/components/Email/ReplyContainer.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import EmailRecipients from '../common/EmailRecipients';
 import { useGlobalContext } from '../../contexts/GlobalContext';
+import RichTextEditor from '../RichTextEditor/RichTextEditor';
 import "./ReplyContainer.css";
 import replyIcon from '../../icons/reply.png';
 import replyAllIcon from '../../icons/replyall.png';
 import forwardIcon from '../../icons/forward.png';
 import dropdownArrow from '../../icons/dropdownarrow.png';
+
+// TEMP
+import styles from "../ComposeEmail/ComposeEmail.module.css";
 
 const ReplyContainer = ({ email, replyType }) => {
   const { loggedInUser } = useGlobalContext();
@@ -30,6 +34,7 @@ const ReplyContainer = ({ email, replyType }) => {
   };
 
   const [recipients, setRecipients] = useState(() => calculateRecipients(replyType));
+  const [content, setContent] = useState({ html: '', plainText: '' });
 
   useEffect(() => {
     setRecipients(calculateRecipients(selectedReplyOption));
@@ -41,13 +46,17 @@ const ReplyContainer = ({ email, replyType }) => {
     { value: 'forward', label: 'Forward', icon: forwardIcon }
   ];
 
-  useEffect(() => {
-    console.log(recipients)
-  }, [recipients])
-
   const getSelectedIcon = () => {
     return options.find(option => option.value === selectedReplyOption)?.icon;
   };
+
+  const handleSend = () => {
+    console.log(content)
+  }
+
+  const handleDelete = () => {
+    console.log('delete')
+  }
 
   return (
     <div style={{
@@ -104,6 +113,103 @@ const ReplyContainer = ({ email, replyType }) => {
             />
           </div>
         </div>
+        <div className="reply-editor-container">
+          <RichTextEditor
+            content={content.html}
+            onChange={(html, plainText) => setContent({ html, plainText })}
+            className="reply-editor"
+          />
+        </div>
+        <div className={styles.composeToolbar}>
+            <div className={styles.sendButtonContainer}>
+              <div
+                aria-label="Send ‪(⌘Enter)‬"
+                role="button"
+                tabIndex="1"
+                style={{
+                  whiteSpace: "nowrap",
+                  textAlign: "center",
+                  verticalAlign: "middle",
+                  boxShadow: "none",
+                  WebkitUserDrag: "none",
+                  lineHeight: "18px",
+                  outline: "none",
+                  padding: "0px 16px",
+                  border: "none",
+                  WebkitBoxAlign: "center",
+                  alignItems: "center",
+                  display: "inline-flex",
+                  WebkitBoxPack: "center",
+                  justifyContent: "center",
+                  position: "relative",
+                  zIndex: 0,
+                  WebkitFontSmoothing: "antialiased",
+                  fontSize: "0.875rem",
+                  letterSpacing: "normal",
+                  backgroundImage: "none",
+                  boxSizing: "border-box",
+                  fontWeight: 500,
+                  height: "36px",
+                  color: "rgb(255, 255, 255)",
+                  margin: "0px",
+                  marginRight: "0px",
+                  maxWidth: "104px",
+                  minWidth: "72px",
+                  cursor: "pointer",
+                  borderRadius: "18px 0px 0px 18px",
+                  userSelect: "none",
+                }}
+                onClick={handleSend}
+              >
+                Send
+              </div>
+              <div
+                className={styles.sendOptionsArrow}
+                aria-expanded="false"
+                aria-haspopup="true"
+                aria-label="More send options"
+                role="button"
+                tabIndex="1"
+                style={{
+                  whiteSpace: "nowrap",
+                  textAlign: "center",
+                  boxShadow: "none",
+                  WebkitUserDrag: "none",
+                  lineHeight: "18px",
+                  outline: "none",
+                  border: "none",
+                  WebkitBoxAlign: "center",
+                  alignItems: "center",
+                  display: "inline-flex",
+                  WebkitBoxPack: "center",
+                  justifyContent: "center",
+                  position: "relative",
+                  zIndex: 0,
+                  WebkitFontSmoothing: "antialiased",
+                  fontFamily: '"Google Sans", Roboto, RobotoDraft, Helvetica, Arial, sans-serif',
+                  fontSize: "0.875rem",
+                  letterSpacing: "normal",
+                  backgroundImage: "none",
+                  boxSizing: "border-box",
+                  fontWeight: 500,
+                  height: "36px",
+                  color: "rgb(255, 255, 255)",
+                  padding: "0px 8px",
+                  minWidth: "24px",
+                  borderLeft: "1px solid rgb(6, 46, 111)",
+                  cursor: "pointer",
+                  borderRadius: "0px 18px 18px 0px",
+                  userSelect: "none",
+                }}
+              >
+                <span className="material-symbols-outlined">arrow_drop_down</span>
+              </div>
+            </div>
+
+            <button className={styles.deleteButton} onClick={handleDelete} title="Delete">
+              <span className="material-symbols-outlined">delete</span>
+            </button>
+          </div>
       </div>
     </div>
   )
