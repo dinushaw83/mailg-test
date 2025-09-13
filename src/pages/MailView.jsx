99b11a4ccaa14b8ff7512bcbe552e3efbeb52c10
Gideon-ka <gideon.o@turing.com>
2025-09-13 11:30:28 +0100

    style: add spacing to text

--- src/pages/MailView.jsx
+++ src/pages/MailView.jsx
@@ -374,8 +374,8 @@ const Inbox = () => {
                                       </div>
                                       <div className="aiG" jslog="108910; u014N:cOuCgd,Kr2w4b,xr6bB; 40:WzFd">
                                         <div className="aiD">
-                                          <span dir="ltr">15%</span>
-                                          of <span dir="ltr">15 GB</span>
+                                          <span dir="ltr">15%&nbsp;</span>
+                                          of <span dir="ltr">15&nbsp;GB&nbsp;</span>
                                           used
                                         </div>
                                         <div className="aiz" role="img" aria-label="Follow link to manage storage" />
