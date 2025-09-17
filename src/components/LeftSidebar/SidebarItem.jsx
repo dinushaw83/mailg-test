681feb46bb893fca984f86f5375201b388d8e10b
Gideon-ka <gideon.o@turing.com>
2025-09-17 15:20:27 +0100

    feat(SidebarItem): add tooltip to sidebar items

--- src/components/LeftSidebar/SidebarItem.jsx
+++ src/components/LeftSidebar/SidebarItem.jsx
@@ -1,3 +1,4 @@
+import { Tooltip } from "@mui/material";
 import React from "react";
 import { NavLink, useMatch } from "react-router-dom";
 
@@ -21,51 +22,64 @@ export default function SidebarItem({ item }) {
   };
 
   return (
-    <NavLink
-      to={`/${item.key}`}
-      end
-      className="J-Ke n0"
-      style={{ textDecoration: "none", color: "inherit" }}
-      aria-label={item.label}
-      onClick={handleClick}
-    >
-      <div className={`aim ${isActive ? "ain" : ""}`}>
-        <div className={`TO n6 ${isActive ? "aBP nZ aiq" : ""}`}>
-          <div
-            className={`TN bzz ah9 ${hsClass(item.key)}`}
-            style={{ marginLeft: 0 }}
-          >
-            <span
-              className={
-                isActive
-                  ? "material-symbols-filled"
-                  : "material-symbols-outlined"
-              }
-              style={{
-                alignItems: "center",
-                display: "flex",
-                flexShrink: 0,
-                fontSize: "20px",
-                justifyContent: "flex-start",
-                marginRight: "18px",
-                opacity: isActive ? 1 : 0.71,
-              }}
+    <Tooltip title={item.label} placement="right" slotProps={{
+      popper: {
+        modifiers: [
+          {
+            name: 'offset',
+            options: {
+              offset: [0, -13],
+            },
+          },
+        ],
+      },
+    }}>
+      <NavLink
+        to={`/${item.key}`}
+        end
+        className="J-Ke n0"
+        style={{ textDecoration: "none", color: "inherit" }}
+        aria-label={item.label}
+        onClick={handleClick}
+      >
+        <div className={`aim ${isActive ? "ain" : ""}`}>
+          <div className={`TO n6 ${isActive ? "aBP nZ aiq" : ""}`}>
+            <div
+              className={`TN bzz ah9 ${hsClass(item.key)}`}
+              style={{ marginLeft: 0 }}
             >
-              {item.icon}
-            </span>
+              <span
+                className={
+                  isActive
+                    ? "material-symbols-filled"
+                    : "material-symbols-outlined"
+                }
+                style={{
+                  alignItems: "center",
+                  display: "flex",
+                  flexShrink: 0,
+                  fontSize: "20px",
+                  justifyContent: "flex-start",
+                  marginRight: "18px",
+                  opacity: isActive ? 1 : 0.71,
+                }}
+              >
+                {item.icon}
+              </span>
 
-            <div className="aio UKr6le">
-              <span className={`nU ${isActive ? "n1" : ""}`}>{item.label}</span>
+              <div className="aio UKr6le">
+                <span className={`nU ${isActive ? "n1" : ""}`}>{item.label}</span>
 
-              {item.key !== "all" &&
-                typeof item.count === "number" &&
-                item.count > 0 && <div className="bsU">{item.count}</div>}
+                {item.key !== "all" &&
+                  typeof item.count === "number" &&
+                  item.count > 0 && <div className="bsU">{item.count}</div>}
+              </div>
+
+              <div className="nL aif" />
             </div>
-
-            <div className="nL aif" />
           </div>
         </div>
-      </div>
-    </NavLink>
+      </NavLink>
+    </Tooltip>
   );
 }
