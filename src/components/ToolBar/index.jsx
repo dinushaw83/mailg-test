4080123fa12f27975b71866f82dc39eabef1462b
Benjamin Mayanja V <vibenjamin6@gmail.com>
2025-09-25 10:01:06 +0300

    Fix spelling for split

--- src/components/ToolBar/index.jsx
+++ src/components/ToolBar/index.jsx
@@ -170,19 +170,19 @@ const ToggleSplitPaneButton = () => {
           anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
         >
           <ActionMenuItem
-            label="No Spit"
+            label="No Split"
             onClick={() => {
               handleSplitPane({ direction: "vertical", showPanel: false });
             }}
           />
           <ActionMenuItem
-            label="Vertical Spit"
+            label="Vertical Split"
             onClick={() => {
               handleSplitPane({ direction: "vertical", showPanel: true });
             }}
           />
           <ActionMenuItem
-            label="Horizontal Spit"
+            label="Horizontal Split"
             onClick={() => {
               handleSplitPane({ direction: "horizontal", showPanel: true });
             }}
