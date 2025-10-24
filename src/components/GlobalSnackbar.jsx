08909bef80896c05614b08a8e164118401e69ae8
gideon-ka <gideon.o@turing.com>
2025-10-24 17:05:26 +0100

    fix: move snackbar back to the left (#111)

--- src/components/GlobalSnackbar.jsx
+++ src/components/GlobalSnackbar.jsx
@@ -26,7 +26,7 @@ export default function GlobalSnackbar() {
       open={snackbar.open}
       autoHideDuration={snackbar.autoHideDuration}
       onClose={handleClose}
-      anchorOrigin={snackbar.anchorOrigin || { vertical: "bottom", horizontal: "right" }}
+      anchorOrigin={snackbar.anchorOrigin || { vertical: "bottom", horizontal: "left" }}
       message={snackbar.message}
       action={
         <React.Fragment>
