8fcccad92856ed682ea37e8eec4bd47b6a8525c3
Bello Promise <bello.d@turing.com>
2026-01-15 12:37:31 +0100

    fix: app crash

--- src/components/InboxView/Attachments.jsx
+++ src/components/InboxView/Attachments.jsx
@@ -296,7 +296,7 @@ export const Attachments = ({ attachments = [] }) => {
 
   const getAttachmentWithPreviewURL = useCallback(
     async (attachment) => {
-      const isRelativeURL = attachment.url.startsWith("/");
+      const isRelativeURL = attachment?.url?.startsWith("/");
       if (isRelativeURL) {
         return { ...attachment, previewURL: attachment.url };
       }
